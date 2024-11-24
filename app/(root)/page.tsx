bd5c6696b4c339904eb98128787e314678c6557d
Aryan <durgearyan9@gmail.com>
2024-11-24 18:45:26 +0530

    First commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import HeaderBox from '@/components/ui/HeaderBox'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          Home
+        </header>
+      </div>
+    </section>
+  )
+}
+
+export default Home
